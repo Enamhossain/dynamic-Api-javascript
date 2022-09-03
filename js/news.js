eb7423f468508b6e8c14bd9c38a6f1a1c3e5f036
Enamhossain <bhaianam9@gmail.com>
2022-09-03 10:06:26 +0600

    js connect

---
+++ js/news.js
@@ -0,0 +1,7 @@
+const loadData =  async() =>{
+    const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await response.json()
+    console.log(data)
+}
+
+loadData()
